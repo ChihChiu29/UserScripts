6ca05756f08113a4175a7048fafb1e7f3178a1d9
ChihChiu29 <chih.chiu.19@gmail.com>
2021-03-28 18:30:25 -0700

    Create igg_games_helper.js

---
+++ igg_games_helper.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         IGG Games Helper
+// @namespace    nobody
+// @version      0.1
+// @match        http://bluemediafiles.com/*
+// @match        https://mega.nz/*
+// @grant        GM_log
+// ==/UserScript==
+
+(function() {
+    GM_log('Welcome to IGG Games Helper!');
+    function runUntil(fn, intervalSec, untilSec) {
+        const intervalId = setInterval(fn, intervalSec * 1000);
+        setTimeout(function() {
+            clearInterval(intervalId);
+        }, untilSec * 1000);
+    }
+
+    if (window.location.hostname === 'bluemediafiles.com') {
+        runUntil(function() {
+            const adsRight = document.querySelector('#ads-right');
+            const button = adsRight.nextElementSibling.nextElementSibling;
+            if (button && button.tagName === 'DIV') {
+                GM_log('Goodbye redirect page!');
+                document.querySelector('form').submit();
+            }
+        }, 5, 60);
+    } else if (window.location.hostname === 'mega.nz') {
+        runUntil(function() {
+            const button = document.querySelector('.download-file');
+            if (button) {
+                GM_log('Start download!');
+                button.click();
+            }
+        }, 5, 30);
+    }
+})();
